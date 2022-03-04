ff1a78942120e1d4553cc51c0c720654075509ec
VARVISHNU08 <91448991+VARVISHNU08@users.noreply.github.com>
2022-03-04 19:59:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/VX2G-x754/model.json', modelReady);
+}
+
+function modelReady(){
+    
+        classifier.classify(gotResults);
+    
+}
+
+function gotResults(error, results){
+    console.log("Got Results");
+}
